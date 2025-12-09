458822ac71b7069ebda48fc2b2ac4171fd0c6507
AeYRfs <148368584+AeYRfs@users.noreply.github.com>
2025-12-09 10:42:54 +0700

    Update TermCard.jsx

--- src/components/TermCard.jsx
+++ src/components/TermCard.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+const TermCard = ({ term, description }) => {
+  return (
+    <div className="term-card">
+      <div className="term-title">{term}</div>
+      <div className="term-desc">{description}</div>
+    </div>
+  );
+};
+
+export default TermCard;
